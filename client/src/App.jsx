0c96200b812c8ff2727e540ae04b13748958f844
obumneme eziamaka <eziamakaobum@yahoo.co.uk>
2022-12-03 09:41:21 +0000

    created post component pulled api and created button function to show all posts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import Posts from './components/Posts'
 
 function App() {
-	const [value, setValue] = useState('/posts')
-	const [data, setData] = useState('')
+  const [value, setValue] = useState("/posts");
+  const [data, setData] = useState("");
+  const [isShown, setIsShown] = useState(false)
+  
+  
+  const handleClick = event =>{
+    setIsShown(current => !current)
+  }
 
-	
+  useEffect(() => {
+    const API_URL = `https://${window.location.hostname}:1338${value}`;
+    fetch(API_URL)
+      .then((t) => t.text())
+      .then((data) => {
+        setData(data);
+      });
+  }, [value]);
 
-	useEffect(() => {
-		const API_URL = `https://${window.location.hostname}:1338${value}`
-		console.log(API_URL)
-		fetch(API_URL)
-			.then((t) => t.text())
-			.then((data) => {
-				setData(data)
-			})
-	}, [value])
-
-	return (
-
-		
-		<div>
-			<h1>Hello World!</h1>
-			<input
-				type="text"
-				placeholder="write the relative URL here to get data"
-				value={value}
-				onChange={(e) => setValue(e.target.value)}
-			/>
-			<pre>{data}</pre>
-		</div>
-	)
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <div>
+         <button onClick={handleClick}>showpost</button>
+         
+         {isShown && <Posts/>}
+          
+        
+         
+        </div>
+      
+     
+  
+      <input
+        type="text"
+        placeholder="write the relative URL here to get data"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      {/* <pre>{data}</pre> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
