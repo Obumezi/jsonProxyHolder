6fb603f57197abe4a455d93b9a6434077d3797cb
obumneme eziamaka <eziamakaobum@yahoo.co.uk>
2022-12-07 12:45:11 +0000

    Update 4 files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
-import Posts from './components/Posts'
+import Posts from "./components/Posts";
+
 
 function App() {
   const [value, setValue] = useState("/posts");
   const [data, setData] = useState("");
-  const [isShown, setIsShown] = useState(false)
-  
-  
-  const handleClick = event =>{
-    setIsShown(current => !current)
-  }
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = (event) => {
+    setIsShown((current) => !current);
+  };
 
   useEffect(() => {
     const API_URL = `https://${window.location.hostname}:1338${value}`;
@@ -24,23 +24,19 @@ function App() {
     <div>
       <h1>Hello World!</h1>
       <div>
-         <button onClick={handleClick}>showpost</button>
-         
-         {isShown && <Posts/>}
-          
-        
-         
-        </div>
+        <button onClick={handleClick}>Post Title</button>
+
+        {isShown && <Posts />}
+      </div>
       
-     
-  
+
       <input
         type="text"
         placeholder="write the relative URL here to get data"
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
-      {/* <pre>{data}</pre> */}
+      <pre>{data}</pre>
     </div>
   );
 }
