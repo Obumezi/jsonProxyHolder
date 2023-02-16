f1b265260cdadfd6f087058caf0a90abf62d99c6
obumneme eziamaka <eziamakaobum@yahoo.co.uk>
2023-02-16 16:53:46 +0000

    created navbar and body text

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Poststyle.css";
 /* import Card from "react-bootstrap/Card"; */
-import Cards from '../components/Cards'
+import Cards from "../components/Cards";
 
 const Posts = () => {
   const [posts, setPost] = useState([]);
@@ -13,19 +13,19 @@ const Posts = () => {
   const fetchPosts = async () => {
     await fetch(`https://${window.location.hostname}:1338/posts`)
       .then((res) => res.json())
-      .then((data) => setPost(data.slice(0,5)));
+      .then((data) => setPost(data.slice(0, 9)));
     /*  .catch((err) => {
         console.log(err);
       }); */
   };
 
-return (
+  return (
     <div className="cardContainer">
-      {posts.map(post => (
+      {posts.map((post) => (
         <Cards key={post.id} post={post} />
       ))}
     </div>
   );
-}
+};
 
 export default Posts;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
